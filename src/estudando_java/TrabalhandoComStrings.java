7e8c2b988521553ac4d572798c96d690e707dd0a
luiscarlos <luiscarlos8pe@gmail.com>
2023-11-17 10:11:07 -0300

    Trabalhando com String

---
+++ src/estudando_java/TrabalhandoComStrings.java
@@ -0,0 +1,20 @@
+package estudando_java;
+
+public class TrabalhandoComStrings {
+	public static void main(String[] args) {
+		String nome = "Maria";
+		int idade = 30;
+		
+		System.out.println(nome + " tem " + idade + " anos ");
+		
+		System.out.println("===================");
+		
+		String n = "Maria";
+		int i = 30;
+		int c = 10;
+		
+		System.out.println(nome + i + c);
+		System.out.println( i + c + nome);
+		System.out.println(nome + (i + c));
+	}
+}
